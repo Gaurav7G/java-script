932659feabec54a7b94299e52ffea4b51d274734
Gaurav7G <edugaurav9918@gmail.com>
2024-10-14 18:25:57 +0000

    configured

---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const accountId="1545"
+let accountEmail="jai@gmail.com"
+accountCity="ballia"
+accountState="UP"
+//console.log(accountId);
+//accountId="1224"  /*constant does not change */
+accountEmail="gaur@gmail.com"
+console.table([accountId,accountEmail,accountCity])
