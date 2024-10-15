25b295a58f8087e55c82e63d1446a661e09c8f76
Gaurav7G <edugaurav9918@gmail.com>
2024-10-15 09:10:31 +0000

    conversionOperations

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -3,3 +3,14 @@ console.log(typeof score);
 let valueInNumber=Number(score) //another variable containing score but it converts to number datatype if score is given in another datatype.
 console.log(typeof valueInNumber);
 console.log(valueInNumber)
+
+//"33"=>33
+// "33abc"=>NaN
+//true =>1 and false=>0
+
+let isLoggedIn=1
+let booleanisLoggedIn= Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+// 1=> true 0=>false
+// ""=>false
+//  "gaurav"=>true
