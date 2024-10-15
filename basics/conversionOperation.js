123fbb9e9cf8be7f042893403a307c175492fb7e
Gaurav7G <edugaurav9918@gmail.com>
2024-10-15 09:01:43 +0000

    conversionOperations

---
+++ basics/conversionOperation.js
@@ -0,0 +1,5 @@
+let score="33absc" //here we want number but user may had given the number in double quotes which make it a string.
+console.log(typeof score);
+let valueInNumber=Number(score) //another variable containing score but it converts to number datatype if score is given in another datatype.
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
